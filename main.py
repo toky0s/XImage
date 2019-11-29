fff0e360a708ac501149b418615e153661183a82
A Xin <truongaxin123@gmail.com>
2019-11-29 22:23:31 +0700

    xử lý đa luồng, refactor code

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import urllib.request
 import time
 import shutil
 import threading
+import logging
 
 
 # unsplash
@@ -167,31 +168,61 @@ class UnsplashUI(Frame):
         self.button_download.image = self.image_icon_download
         self.button_download.grid(row=7, column=1)
 
-        self.text_infor = Text(self)
-        self.text_infor['width'] = 50
-        self.text_infor['height'] = 12
-        self.text_infor['bg'] = '#0a3d62'
-        self.text_infor['fg'] = 'white'
-        self.text_infor.grid(row=8, column=0, columnspan=3)
-
     def startDownload(self):
         # check folder
         if self.var_folder_name.get() == '':
             messagebox.showwarning('Thông báo','Bạn chưa chọn nơi lưu trữ!')
             return
         else:
-            self.text_infor.delete(1.0,'end')
             self.lock = threading.Lock()
             self.th_sendRequest = threading.Thread(target=self.getListUnsplashImages)
             self.th_sendRequest.start()
+            
+            logging.info('Initialize the Download UI')
+            self.toplevel_info = Toplevel(self)
+            self.toplevel_info.title('Downloading...')
+
+            self.var_progress = StringVar()
+            self.label_progress = Label(self.toplevel_info)
+            self.label_progress['textvariable'] = self.var_progress
+            self.label_progress.pack(fill='x')
+
+            self.progress_bar = Progressbar(self.toplevel_info)
+            self.progress_bar['length']=100
+            self.progress_bar['orient']='horizontal'
+            self.progress_bar['mode']='determinate'
+            self.progress_bar['value'] = 0
+            self.progress_bar.pack(fill='x')
+
+            self.label_image = Label(self.toplevel_info)
+            self.label_image.pack(fill='x')
+
             self.th_download = threading.Thread(target=self.download)
             self.th_download.start()
 
     def download(self):
         self.lock.acquire()
+        per = 0
         for image in self.listUnsplashImage:
-            i = image.downloadThisImage(self.var_quality_rb.get(),self.var_folder_name.get())
-            self.text_infor.insert('end',f'--> {i}\n')
+            path = image.downloadThisImage(self.var_quality_rb.get(),self.var_folder_name.get())
+
+            per = per + 100/len(self.listUnsplashImage)
+            self.var_progress.set('{}%'.format(int(per)))
+            self.progress_bar['value'] = per
+
+            self.image_downloaded = Image.open(path)
+            self.image_complete = ImageTk.PhotoImage(self.image_downloaded)
+
+
+            self.label_image['image'] = self.image_complete
+            self.label_image.image = self.image_complete
+        
+        # release data
+        logging.info('show warning')
+        messagebox.showinfo('Thông báo','Đã tải xong !')
+        self.toplevel_info.destroy()
+        self.listUnsplashImage = []
+
         self.lock.release()
 
     def disable_order_by_for_random(self):
@@ -224,7 +255,6 @@ class UnsplashUI(Frame):
             request = 'https://unsplash.com/napi/photos/random'
             params = {'count': self.var_amount.get()}
             r = requests.get(request, params=params)
-            self.text_infor.insert('end',f'status code: {r.status_code}\n')
             data = json.loads(r.text)
         elif self.var_name.get() == '':
             request = 'https://unsplash.com/napi/photos'
@@ -233,7 +263,6 @@ class UnsplashUI(Frame):
                 'per_page': self.var_amount.get()
             }
             r = requests.get(request, params=params)
-            self.text_infor.insert('end',f'status code: {r.status_code}\n')
             data = json.loads(r.text)
         else:
             # call searchPhoto request
@@ -246,7 +275,6 @@ class UnsplashUI(Frame):
             r = requests.get(request, params=params)
             data = json.loads(r.text)
             data = data['results']
-            self.text_infor.insert('end',f'status code: {r.status_code}\n')
 
         for i in data:
             img = UnsplashImage(i)
@@ -265,7 +293,6 @@ class GraphicRiverUI(Frame):
 
     def setupUI(self):
 
-        # row 0
         self.label_url = Label(self, text='Url:')
         self.label_url.grid(row=0, column=0, sticky='w')
 
@@ -273,19 +300,19 @@ class GraphicRiverUI(Frame):
         self.entry_url = Entry(self, textvariable=self.var_url)
         self.entry_url.grid(row=0, column=1, sticky='we')
 
-        # row 1
         self.label_save = Label(self, text='Save:')
         self.label_save.grid(row=1, column=0, sticky='w')
 
         self.var_folder_name = StringVar()
-        self.entry_save = Entry(self, textvariable=self.var_folder_name)
+        self.entry_save = Entry(self)
+        self.entry_save['textvariable'] = self.var_folder_name
         self.entry_save.grid(row=1, column=1, sticky='we')
 
-        self.button_browse = Button(
-            self, text='Browse', command=self.choice_folder)
+        self.button_browse = Button(self)
+        self.button_browse['text']='Browse'
+        self.button_browse['command'] = self.choice_folder
         self.button_browse.grid(row=1, column=2, sticky='we')
 
-        # row 2
         self.label_amount = Label(self, text='Amount:')
         self.label_amount.grid(row=2, column=0, sticky='w')
 
@@ -294,47 +321,24 @@ class GraphicRiverUI(Frame):
         self.entry_amount.grid(row=2, column=1, sticky='we')
 
         # row 3
-        self.button_download = Button(
-            self, text='Download', command=self.change_ui)
+        self.button_download = Button(self)
+        self.button_download['text'] = 'Download'
+        self.button_download['command'] = self.startDownload
         self.button_download.grid(row=3, column=1, sticky='we')
 
-        self.button_usage = Button(
-            self, text='Usage', command=self.show_user_manual)
+        self.button_usage = Button(self)
+        self.button_usage['text']='Usage'
+        self.button_usage['command'] = self.show_user_manual
         self.button_usage.grid(row=3, column=2, sticky='we')
 
-        # HIDE===========================================
-        # row 4
-        self.label_download = Label(self, text='Download')
-        self.label_download.config(
-            anchor='center', background='#27ae60', foreground='white')
-
-        # row 5
-        self.var_progress = StringVar()
-        self.label_progress = Label(self, textvariable=self.var_progress)
-
-        self.progress_bar = Progressbar(self)
-        self.progress_bar.config(
-            length=100, orient='horizontal', mode='determinate')
-
-        # row 6
-        self.label_image = Label(self, text='hi')
-
     def choice_folder(self):
+        logging.info('Choose a folder to save')
         dialog_choice_folder = filedialog.askdirectory()
         self.var_folder_name.set(dialog_choice_folder)
 
     def get_name_presentation(self):
         self.name_presentation = 'Name Presentation'
-        # process it
         return self.name_presentation
-
-    def change_ui(self):
-        self.label_download.grid(row=4, column=0, columnspan=3, sticky='we')
-        self.label_progress.grid(row=5, column=0)
-        self.progress_bar.grid(row=5, column=1, columnspan=2, sticky='we')
-        self.label_image.grid(row=6, column=0, columnspan=3)
-        self.master.after(1000, self.get_list_tag_a)
-        self.master.after(1000, self.download)
 
     def __has_a_http_jpg(self, tag):
         # check image's link is https://www.somthing.jpg
@@ -342,6 +346,8 @@ class GraphicRiverUI(Frame):
             return True
 
     def get_list_tag_a(self):
+        '''Trả về một list chứa các link để tải ảnh'''
+        logging.info('Get list tag <a>')
         # return a list, which containe <a> tags
         r = requests.get(self.var_url.get())
         if r.status_code == 200:
@@ -354,63 +360,67 @@ class GraphicRiverUI(Frame):
             for i in list_tag_a:
                 self.list_tag_a.append(i['href'])
 
+    def startDownload(self):
+        logging.info('Prepair data to download')
+        # prepair data
+        self.dir_name = self.var_url.get().split('/')[4]
+        if os.path.exists(self.var_folder_name.get()+'/'+self.dir_name):
+            shutil.rmtree(self.var_folder_name.get()+'/'+self.dir_name)
+
+        self.full_path_save = self.var_folder_name.get()+'/'+self.dir_name
+        os.mkdir(self.full_path_save)
+        self.get_list_tag_a()
+
+        logging.info('Initialize the Download UI')
+        self.toplevel_info = Toplevel(self)
+        self.toplevel_info.title('Downloading...')
+
+        self.var_progress = StringVar()
+        self.label_progress = Label(self.toplevel_info)
+        self.label_progress['textvariable'] = self.var_progress
+        self.label_progress.pack(fill='x')
+
+        self.progress_bar = Progressbar(self.toplevel_info)
+        self.progress_bar['length']=100
+        self.progress_bar['orient']='horizontal'
+        self.progress_bar['mode']='determinate'
+        self.progress_bar['value'] = 0
+        self.progress_bar.pack(fill='x')
+
+        self.label_image = Label(self.toplevel_info)
+        self.label_image.pack(fill='x')
+
+        self.th_download = threading.Thread(target=self.download)
+        self.th_download.start()
+
     def download(self):
-        # https:, ,graphicriver.net, item, i9-template-system, 10955645
-        # create dir, where contain images
-        dir_name = self.var_url.get().split('/')[4]
-        if os.path.exists(self.var_folder_name.get()+'/'+dir_name):
-            shutil.rmtree(self.var_folder_name.get()+'/'+dir_name)
-
-        full_path_save = self.var_folder_name.get()+'/'+dir_name
-        os.mkdir(full_path_save)
-
+        logging.info('downloading')
         per = 0
         for i in self.list_tag_a:
             name_image = i.split('/')[-1]
-            request.urlretrieve(i, full_path_save+'/'+name_image)
-
-            image_downloaded = Image.open(full_path_save+'/'+name_image)
-            width = int(self.winfo_width())
-            height = int(width*image_downloaded.height /
-                         image_downloaded.width)
-
-            image_complete = ImageTk.PhotoImage(
-                image_downloaded.resize((width, height), Image.ANTIALIAS))
-            self.label_image.config(image=image_complete)
-            self.label_image.image = image_complete
-
+            request.urlretrieve(i, self.full_path_save+'/'+name_image)
+            
             per = per + 100/self.var_amount.get()
-            self.var_progress.set('{}%'.format(per))
-            self.progress_bar['value'] += per
+            self.var_progress.set('{}%'.format(int(per)))
+            self.progress_bar['value'] = per
 
-            self.progress_bar.update_idletasks()
+            self.image_downloaded = Image.open(self.full_path_save+'/'+name_image)
+            self.image_complete = ImageTk.PhotoImage(self.image_downloaded)
+
+
+            self.label_image['image'] = self.image_complete
+            self.label_image.image = self.image_complete
+
+        # release data
+        logging.info('show warning')
+        messagebox.showinfo('Thông báo','Đã tải xong !')
+        self.toplevel_info.destroy()
+        self.list_tag_a = []
 
     def show_user_manual(self):
         # User manual
-        self.usage_popup_window = Toplevel(self)
-        self.usage_frame = FrameUserManual(self.usage_popup_window)
-        self.usage_frame.pack()
+        print('show manual')
 
-
-class FrameUserManual(Frame):
-
-    def __init__(self, master=None):
-        super().__init__(master=master)
-        self.master = master
-        self.setupUI()
-
-    def setupUI(self):
-        self.label_title = Label(
-            self.master, text='Usage', background='#8854d0', anchor='center')
-        self.label_title.pack(fill='x')
-
-        self.image_graphicriver = ImageTk.PhotoImage(
-            file='image/graphicriver.png')
-        self.text = 'Copy path of a template, and paste it into this entry'
-        self.label_text = Label(
-            self.master, text=self.text, image=self.image_graphicriver, compound='left')
-        self.label_text.image = self.image_graphicriver
-        self.label_text.pack()
 
 
 class MyApp(Frame):
